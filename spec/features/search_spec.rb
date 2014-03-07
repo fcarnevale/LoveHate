1146f935ab58ba3c9f30d49f891ff6a42b1e4272
Frank Carnevale <frank.carnevale@gmail.com>
2014-03-06 20:49:34 -0500

    First passing scenario, but possible bug

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
+require 'vcr'
+require 'vcr_helper'
 
 feature 'Users can learn how good something is' do
   scenario 'User compares two terms' do
-
     terms = ["microsoft", "apple"]
 
-    terms.each do |term|
-      @scores ||= {}
-      @scores[term] = LoveScore.for_term(term)
-    end
+    VCR.use_cassette("user-compares-two-terms") do
+      terms.each do |term|
+        @scores ||= {}
+        @scores[term] = LoveScore.for_term(term)
+      end
 
+      @scores["apple"].should be > @scores["microsoft"]
+    end
   end
 end
